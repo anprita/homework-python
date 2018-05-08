b76c350b75652370350cfb643eeb58944c2c5ae4
Prita An <prita.an@gmail.com>
2018-05-08 17:37:26 -0400

    modify code to generate the wrong graph

--- index.py
+++ index.py
@@ -2,8 +2,8 @@
 
 import glob
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 #import seaborn as sns
 import sys
 
@@ -20,20 +20,20 @@ def count_kmers(seq, k):
 #function to find the wrong DNA
 def check_seq_format(seq):
     bad_chars = {}
-    for l in seq:
-        if l not in 'AGCT':
-            if l in bad_chars: bad_chars[ l ] += 1
-            else: bad_chars[ l ] = 1
+    for i in seq:
+        if i not in 'AGCT':
+            if i in bad_chars: bad_chars[ i ] += 1
+            else: bad_chars[ i ] = 1
     if bad_chars != {}: return bad_chars
 
 #function to export a table(kmers, observed, possible) in .csv format per each sequence name
-def generate_file(detail_data, seq_name):
-    data = pd.DataFrame([[i[0], i[1], i[2]] for i in detail_data], columns=['k-mers', 'observed', 'possible'])    
-    data.to_csv('output/data/'+seq_name+'.csv', index=False)
+def generate_file(file_name, detail_data, seq_name):
+    data = pd.DataFrame([[i[0], i[1], i[2]] for i in detail_data], columns=['k-mers', 'observed', 'possible'])
+    data.to_csv('output/data/'+ file_name + '_' + seq_name+'.csv', index=False)
     #return data
 
 #function to generate a linguistic complexity graph of all sequence name
-def generate_complexity_graph(file_name, seq_name_list, linguistic_complexity_list):    
+def generate_complexity_graph(file_name, seq_name_list, linguistic_complexity_list):       
     plt.plot(seq_name_list, linguistic_complexity_list, 'ro')
     plt.xlabel('Sequence Name')
     plt.ylabel('Linguistic Complexity')
@@ -66,6 +66,14 @@ def generate_kmers_graph(file_name, seq_name_list, possible_total_list, observed
     graph_name = file_name + '_kmers.png'
     plt.savefig('output/image/'+graph_name)
 
+#function to generate wrong dana per each sequence name 
+def generate_wrong_dna_graph(file_name, data, seq_name):
+    plt.clf()
+    plt.bar(range(len(data)), list(data.values()), align='center')
+    plt.xticks(range(len(data)), list(data.keys()))
+    graph_name = 'wrong_dna_'+ file_name + '_' + seq_name + '.png'
+    plt.savefig('output/image/'+graph_name)
+
 #main function
 if __name__ == "__main__":
     #get a input variable as a file name
@@ -73,6 +81,7 @@ if __name__ == "__main__":
     if file_name in glob.glob('*.fasta'):
         f = open(file_name,'r')
         seq = f.readlines()
+        file_name = file_name.replace('.', '_')
         seq_name_list = []
         observed_total_list = []
         possible_total_list = []
@@ -86,46 +95,48 @@ if __name__ == "__main__":
                 else:
                     seq = line.rstrip()
                     #check per each sequence name, the sequence format
-                    seq_format = check_seq_format(seq)                    
-                    if seq_format != None:
-                        print('fail to generate kmers-table for ', seq_name, ' due to invalid sequence format, detail:', seq_format) 
-                    else:
-                        k_list = []
-                        possible_list = []
-                        observed_list = []
-                        for k in range(1,len(seq)+1):
-                            if k==1:
-                                #exception only for k=1, possible kmers always 4 (A,C,T,G)
-                                possible = 4
-                            else:
-                                #get the possible kmers
-                                possible = len(seq) - k + 1
-                            #get the kmers
-                            counts = count_kmers(seq, k)
-                            #print(counts)
-                            #get the observed kmers                            
-                            observed = len(counts)
-                            k_list.append(k);
-                            possible_list.append(possible)                          
-                            observed_list.append(observed)
-                        #get the total possible kmers                        
-                        possible_total = sum(possible_list)  
-                        possible_list.append(possible_total)
-                        possible_total_list.append(possible_total)
-                        #get the total observed kmers
-                        observed_total = sum(observed_list)
-                        observed_list.append(observed_total)
-                        observed_total_list.append(observed_total)
-                        k_list.append('Total'); 
-                        #merge kmers-data per each sequence name
-                        detail_data = list(zip(k_list, observed_list, possible_list))
-                        #generate file per each sequence name            
-                        detail = generate_file(detail_data, seq_name)
-                        #get the linguistic complexity
-                        linguistic_complexity = observed_total/possible_total
-                        linguistic_complexity_list.append(linguistic_complexity) 
+                    seq_format = check_seq_format(seq)  
+                    k_list = []
+                    possible_list = []
+                    observed_list = []
+                    for k in range(1,len(seq)+1):
+                        if k==1:
+                            #exception only for k=1, possible kmers always 4 (A,C,T,G)
+                            possible = 4                        
+                        #get the possible kmers
+                        possible = len(seq) - k + 1
+                        #get the kmers
+                        counts = count_kmers(seq, k)
+                        #print(counts)
+                        #get the observed kmers  
+                        observed = len(counts)
+                        k_list.append(k)
+                        possible_list.append(possible)                          
+                        observed_list.append(observed)
+                    #get the total possible kmers                        
+                    possible_total = sum(possible_list)  
+                    possible_list.append(possible_total)
+                    possible_total_list.append(possible_total)
+                    #get the total observed kmers
+                    observed_total = sum(observed_list)
+                    observed_list.append(observed_total)
+                    observed_total_list.append(observed_total)
+                    k_list.append('Total'); 
+                    #merge kmers-data per each sequence name
+                    detail_data = list(zip(k_list, observed_list, possible_list))
+                    #print(detail_data)
+                    #generate file per each sequence name                      
+                    detail = generate_file(file_name, detail_data, seq_name)
+                    #get the linguistic complexity
+                    linguistic_complexity = observed_total/possible_total
+                    linguistic_complexity_list.append(linguistic_complexity)
+        #print(linguistic_complexity_list) 
         summary_linguistic_complexity = generate_complexity_graph(file_name, seq_name_list, linguistic_complexity_list)
-        summary_kmers = generate_kmers_graph(file_name, seq_name_list, possible_total_list, observed_total_list)
-        print('succeed to generate files and graphs for ', seq_name_list) 
+        if seq_format != None:
+            wrong_seq_graph = generate_wrong_dna_graph(file_name, seq_format, seq_name)
+            print('you have wrong dna in ', seq_name, ' detail:', seq_format)        
+        summary_kmers = generate_kmers_graph(file_name, seq_name_list, possible_total_list, observed_total_list)           
+        print('succeed to generate files and graphs') 
+        
     else:
         print('file should be in fasta format')
